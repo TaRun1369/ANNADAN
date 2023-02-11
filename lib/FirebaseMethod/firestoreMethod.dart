173a14bcd19b46d4da5acc228032bba7893243a9
Darshan <darhain101@gmail.com>
2023-02-11 18:09:19 +0530

    admin app

---
+++ lib/FirebaseMethod/firestoreMethod.dart
@@ -0,0 +1,37 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:food_management/Model/foodData.dart';
+import 'package:uuid/uuid.dart';
+
+class FireStoreMethods {
+  final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
+
+  Future<void> removeEntry(String productId) async {
+    try {
+      _firebaseFirestore.collection('Items').doc(productId).delete();
+    } catch (err) {
+      print(err.toString());
+    }
+  }
+
+  Future<void> addEntry(String foodItem, String location, String shopName,
+      double price, String emailId, int foodAge, int foodQuantity) async {
+    try {
+      String productId = const Uuid().v1();
+      FoodData foodData = FoodData(
+          foodItem: foodItem,
+          location: location,
+          shopName: shopName,
+          price: price,
+          emailId: emailId,
+          foodAge: foodAge,
+          foodQuantity: foodQuantity,
+          productId: productId);
+      _firebaseFirestore
+          .collection('Items')
+          .doc(productId)
+          .set(foodData.toJson());
+    } catch (err) {
+      print(err.toString());
+    }
+  }
+}
